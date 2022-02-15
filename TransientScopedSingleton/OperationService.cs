875225d6b1986b6329dfb2b8d5590a9af331ac8f
Idris Wonuola <Idris.wonuola@wemabank.com>
2022-02-15 17:08:13 +0100

    Add project files.

---
+++ TransientScopedSingleton/OperationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransientScopedSingleton
+{
+	public class OperationService :ITransientService, ISingletonService, IScopedService
+	{
+        Guid id;
+        public OperationService()
+        {
+            id = Guid.NewGuid();
+        }
+        public Guid GetOperationID()
+        {
+            return id;
+        }
+    }
+}
